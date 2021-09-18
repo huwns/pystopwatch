509e160af55be97368bf8f3b39c862ecfe09ccd5
huwns <test@gmail.com>
2021-09-19 05:17:08 +0900

    make description luxury

--- pystopwatch/__main__.py
+++ pystopwatch/__main__.py
@@ -7,7 +7,17 @@ from pystopwatch import Stopwatch
 
 def main():
     parser = argparse.ArgumentParser(
-        description="PyStopwatch", prog="python -m pystopwatch"
+        description=r"""PyStopwatch - Simple GUI Stopwatch
++----------------------------------------------------------------------------------+
+|__________         _________ __                                __         .__     |
+|\______   \___.__./   _____//  |_  ____ ________  _  _______ _/  |_  ____ |  |__  |
+| |     ___<   |  |\_____  \\   __\/  _ \\____ \ \/ \/ /\__  \\   __\/ ___\|  |  \ |
+| |    |    \___  |/        \|  | (  <_> )  |_> >     /  / __ \|  | \  \___|   Y  \|
+| |____|    / ____/_______  /|__|  \____/|   __/ \/\_/  (____  /__|  \___  >___|  /|
+|           \/            \/             |__|                \/          \/     \/ |
++----------------------------------------------------------------------------------+""",
+        prog="python -m pystopwatch",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument("--interval", help="interval of refreshing time.[ms]", type=int)
     args = parser.parse_args()
